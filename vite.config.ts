0132eabdd75de703160357e850debdf58ae7c5eb
hdfiresky <hdfiresky@gmail.com>
2025-08-14 14:52:55 +0530

    feat: pwa deploy files and modifications

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,58 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import path from "path";
+import { VitePWA } from 'vite-plugin-pwa'; 
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: '/asset-generator/',
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+  const env = loadEnv(mode, ".", "");
+  return {
+    base: "/asset-generator/",
+    define: {
+      "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: [
+          "favicon.svg",
+          "robots.txt",
+          "apple-touch-icon.png",
+          "pwa-192x192.png",
+          "pwa-512x512.png",
+        ],
+        manifest: {
+          name: "Dramaverse",
+          short_name: "Dramaverse",
+          start_url: "/dramaverse/",
+          scope: "/dramaverse/",
+          display: "standalone",
+          background_color: "#ffffff",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "/dramaverse/pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/dramaverse/pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+            {
+              src: "/dramaverse/pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any maskable",
+            },
+          ],
+        },
+      }),
+    ],
+  };
 });
