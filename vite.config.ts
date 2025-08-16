cad71145bd5fef60ab1f2ea094dd1f7820d10b8d
hdfiresky <hdfiresky@gmail.com>
2025-08-16 19:09:04 +0530

    fix: adding vite config

--- vite.config.ts
+++ vite.config.ts
@@ -28,24 +28,24 @@ export default defineConfig(({ mode }) => {
         manifest: {
           name: "asset-generator",
           short_name: "Launch",
-          start_url: "/",
-          scope: "/",
+          start_url: "asset-generator/",
+          scope: "asset-generator/",
           display: "standalone",
           background_color: "#ffffff",
           theme_color: "#ffffff",
           icons: [
             {
-              src: "asset-generator/pwa-192x192.png",
+              src: "/asset-generator/pwa-192x192.png",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "asset-generator/pwa-512x512.png",
+              src: "/asset-generator/pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
             },
             {
-              src: "asset-generator/pwa-512x512.png",
+              src: "/asset-generator/pwa-512x512.png",
               sizes: "512x512",
               type: "image/png",
               purpose: "any maskable",
